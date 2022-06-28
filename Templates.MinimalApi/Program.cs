657cd24efd0b2b5d27584700beca8a2496a461f0
James Purcell <jamescpurcell@outlook.com>
2022-06-27 19:47:59 -0500

    async test

--- Templates.MinimalApi/Program.cs
+++ Templates.MinimalApi/Program.cs
@@ -4,4 +4,18 @@ var app = builder.Build();
 app.MapGet("get-example", () => "Hello from get!");
 app.MapPost("post-example", () => "Hello from post!");
 
+app.MapGet("ok-object", () => Results.Ok(new
+{
+  Name = "James Purcell"
+}));
+
+app.MapGet("slow-request", async () =>
+{
+  await Task.Delay(1000);
+  return Results.Ok(new
+  {
+    Name = "James Purcell"
+  });
+});
+
 app.Run();
