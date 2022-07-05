3ce0aa19cea788299aa12bb7b63eff68a1b41b61
James Purcell <jamescpurcell@outlook.com>
2022-07-05 16:01:25 -0500

    parameter binding

---
+++ Templates.MinimalApi/MapPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace Templates.MinimalApi;
+
+public class MapPoint
+{
+  public double Latitude { get; set; }
+  public double Longitude { get; set; }
+
+  public static async ValueTask<MapPoint?> BindAsync(
+    HttpContext context, ParameterInfo parameterInfo)
+  {
+    var rawRequestBody = await new StreamReader(context.Request.Body)
+      .ReadToEndAsync();
+
+    try
+    {
+      var splitValue = rawRequestBody?.Split(',').Select(double.Parse).ToArray();
+      return new MapPoint
+      {
+        Latitude = splitValue![0],
+        Longitude = splitValue[1]
+      };
+
+    }
+    catch (Exception)
+    {
+      return null;
+    }
+  }
+}
--- Templates.MinimalApi/Program.cs
+++ Templates.MinimalApi/Program.cs
@@ -3,99 +3,19 @@ using System.Security.Claims;
 using Templates.MinimalApi;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<PeopleService>();
 builder.Services.AddSingleton<GuidGenerator>();
 var app = builder.Build();
 
-// :int prevents string routes as just a 404 - not found 
-app.MapGet("get-params/{age:int}",(int age) =>
+//location?latlong=1.65,0.10
+app.MapGet("map-point",(MapPoint? point) =>
 {
-  return $"Age provided was {age}";
+  return Results.Ok(point);
 });
 
-// :regex prevents special chars in the route as a 404 - not found
-app.MapGet("cars/{carId:regex(^[a-z0-9]+$)}", (string carId) =>
+// or the body of a POST
+app.MapPost("map-point",(MapPoint point) =>
 {
-  return $"Car id provided was: {carId}";
-});
-
-// limits the length of the route object
-app.MapGet("books/{isbn:length(13)}", (string isbn) =>
-{
-  return $"ISBN was: {isbn}";
-});
-
-// since route doesn't have searchTerm, it thinks it's a query string.
-app.MapGet("people/search",
-  (string? searchTerm, PeopleService peopleService) =>
-{
-  if (searchTerm is null)
-  {
-    Results.NotFound();
-  }
-
-  var results = peopleService.Search(searchTerm);
-  return Results.Ok(results);
-});
-
-app.MapGet("mix/{routeParam}", (
-  [FromRoute] string routeParam,
-  [FromQuery(Name = "query")] int queryParam,
-  [FromServices] GuidGenerator guidGenerator,
-  [FromHeader(Name = "Accept-Encoding")] string encoding) =>
-{
-  return $"{routeParam} {queryParam} {guidGenerator} {encoding}";
-});
-
-app.MapPost("people", ([FromBody] Person person) =>
-{
-  return Results.Ok(person);
-});
-
-app.MapGet("httpcontext-1", async context =>
-{
-  await context.Response.WriteAsync("Hello from HttpContext 1");
-});
-
-app.MapGet("httpcontext-2", async (HttpContext context) =>
-{
-  await context.Response.WriteAsync("Hello from HttpContext 2");
-});
-
-
-app.MapGet("http", (HttpRequest request,HttpResponse response) =>
-{
-  return Results.Ok();
-});
-
-app.MapGet("claims", (ClaimsPrincipal user) =>
-{
-  
-});
-
-app.MapGet("cancel", (CancellationToken token) =>
-{
-  token.ThrowIfCancellationRequested();
-  return Results.Ok();
+  return Results.Ok(point);
 });
 
 app.Run();
-
-/*public record Person(string FullName);
-
-public class PeopleService
-{
-  private readonly List<Person> _people = new()
-  {
-    new Person("James Purcell"),
-    new Person("Chloe Porter"),
-    new Person("Lee Kennedy")
-  };
-
-
-  public IEnumerable<Person> Search(string searchTerm)
-  {
-    return _people.Where(x => x.FullName.Contains(
-      searchTerm, StringComparison.OrdinalIgnoreCase));
-  }
-}*/
