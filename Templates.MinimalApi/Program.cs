ea9fe0e551493c2b289001a756236fc66749b967
James Purcell <jamescpurcell@outlook.com>
2022-07-06 18:12:57 -0500

    added more examples for response types

--- Templates.MinimalApi/Program.cs
+++ Templates.MinimalApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Text;
 using Templates.MinimalApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,5 +18,24 @@ app.MapPost("map-point",(MapPoint point) =>
 {
   return Results.Ok(point);
 });
+
+app.MapGet("json-raw-obj", () => new { message = "Hello" });
+app.MapGet("ok-obj", () => Results.Ok(new { message = "Hello" }));
+app.MapGet("json-obj", () => Results.Json(new { message = "Hello" }));
+app.MapGet("text-string", () => Results.Text("Hello"));
+
+app.MapGet("stream-result", () =>
+{
+  var memoryStream = new MemoryStream();
+  var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8);
+  streamWriter.Write("Hello");
+  streamWriter.Flush();
+  memoryStream.Seek(0, SeekOrigin.Begin);
+  return Results.Stream(memoryStream);
+});
+
+app.MapGet("redirect", () => Results.Redirect("https://bing.com"));
+
+app.MapGet("download", () => Results.File("C:\\Temp\\Test.txt"));
 
 app.Run();
