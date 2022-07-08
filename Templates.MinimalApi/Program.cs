9e256a09c816d08f2814269d044198fd6c07dc52
James Purcell <jamescpurcell@outlook.com>
2022-07-08 09:55:41 -0500

    swagger example

--- Templates.MinimalApi/Program.cs
+++ Templates.MinimalApi/Program.cs
@@ -1,11 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using System.Text;
 using Templates.MinimalApi;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<GuidGenerator>();
+
+// Swashbuckle specific services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// middleware setup for swagger at https://localhost/swagger
+app.UseSwagger(options => { });
+app.UseSwaggerUI(options => { });
 
 app.MapGet("Logging", (ILogger<Program> logger) =>
 {
