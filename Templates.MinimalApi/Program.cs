f306469788fb1d80838c41f18bf15f5aee3a9afe
James Purcell <jamescpurcell@outlook.com>
2022-07-08 09:41:44 -0500

    removing old test endpoints

--- Templates.MinimalApi/Program.cs
+++ Templates.MinimalApi/Program.cs
@@ -7,35 +7,4 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<GuidGenerator>();
 var app = builder.Build();
 
-//location?latlong=1.65,0.10
-app.MapGet("map-point",(MapPoint? point) =>
-{
-  return Results.Ok(point);
-});
-
-// or the body of a POST
-app.MapPost("map-point",(MapPoint point) =>
-{
-  return Results.Ok(point);
-});
-
-app.MapGet("json-raw-obj", () => new { message = "Hello" });
-app.MapGet("ok-obj", () => Results.Ok(new { message = "Hello" }));
-app.MapGet("json-obj", () => Results.Json(new { message = "Hello" }));
-app.MapGet("text-string", () => Results.Text("Hello"));
-
-app.MapGet("stream-result", () =>
-{
-  var memoryStream = new MemoryStream();
-  var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8);
-  streamWriter.Write("Hello");
-  streamWriter.Flush();
-  memoryStream.Seek(0, SeekOrigin.Begin);
-  return Results.Stream(memoryStream);
-});
-
-app.MapGet("redirect", () => Results.Redirect("https://bing.com"));
-
-app.MapGet("download", () => Results.File("C:\\Temp\\Test.txt"));
-
 app.Run();
