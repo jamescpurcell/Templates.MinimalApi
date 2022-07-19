940baad8c741ef952b3729e506ac98990145ed07
James Purcell <jamescpurcell@outlook.com>
2022-07-19 13:25:39 -0500

    added endpoint for isbn

--- Templates.MinimalApi/Program.cs
+++ Templates.MinimalApi/Program.cs
@@ -62,5 +62,11 @@ app.MapGet("books", async (IBookService bookService) =>
   return Results.Ok(books);
 });
 
+app.MapGet("books/{isbn}", async (string isbn, IBookService bookService) =>
+{
+  var book = await bookService.GetByIsbnAsync(isbn);
+  return book is not null ? Results.Ok(book) : Results.BadRequest();
+});
+
 // application start
 app.Run();
--- Templates.MinimalApi/Services/BookService.cs
+++ Templates.MinimalApi/Services/BookService.cs
@@ -38,9 +38,11 @@ public class BookService : IBookService
     return await connection.QueryAsync<Book>("SELECT * FROM Books");
   }
 
-  public Task<Book?> GetByIsbnAsync(string isbn)
+  public async Task<Book?> GetByIsbnAsync(string isbn)
   {
-    throw new NotImplementedException();
+    using var connection = await _connectionFactory.CreateConnectionAsync();
+    return connection.QuerySingleOrDefault<Book>(
+      "SELECT * FROM Books WHERE Isbn = @Isbn", new { Isbn = isbn });
   }
 
   public Task<IEnumerable<Book>> SearchByTitleAsync(string searchTerm)
