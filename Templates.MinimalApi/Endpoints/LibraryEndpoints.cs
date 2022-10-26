acefdaffb4a5ccde6a761509d62b9a8431fe4329
James Purcell <jamescpurcell@outlook.com>
2022-10-25 19:44:59 -0500

    JCP - updated the library endpoints to be more delegatey.

--- Templates.MinimalApi/Endpoints/LibraryEndpoints.cs
+++ Templates.MinimalApi/Endpoints/LibraryEndpoints.cs
@@ -6,103 +6,47 @@ using Templates.MinimalApi.Auth;
 using Templates.MinimalApi.Endpoints.Internal;
 using Templates.MinimalApi.Models;
 using Templates.MinimalApi.Services;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Primitives;
 
 namespace Templates.MinimalApi.Endpoints;
 
 public class LibraryEndpoints : IEndpoints
 {
+  private const string ContentType = "application/json";
+  private const string Tag = "Books";
+  private const string BaseRoute = "Books";
   public static void DefineEndpoints(IEndpointRouteBuilder app)
   {
-    app.MapPost("books",
-      [Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
-      async (Book book, IBookService bookService,
-      IValidator<Book> validator, LinkGenerator linker,
-      HttpContext httpContext) =>
-    {
-      var validationResult = await validator.ValidateAsync(book);
-      if (!validationResult.IsValid)
-      {
-        return Results.BadRequest(validationResult.Errors);
-      }
+    app.MapPost(BaseRoute, CreateBookAsync)
+      .WithName("CreateBook")
+      .Accepts<Book>(ContentType)
+      .Produces<Book>(201)
+      .Produces<IEnumerable<ValidationFailure>>(400)
+      .WithTags(Tag);
 
-      var created = await bookService.CreateAsync(book);
-      if (!created)
-      {
-        return Results.BadRequest(new List<ValidationFailure>
-      {
-        new ("Isbn","A book with this ISBN-13 already exists")
-      });
-      }
-
-      // nullable at the end
-      var path = linker.GetPathByName("GetBook", new { isbn = book.Isbn })!;
-      //var locationUri = linker.GetUriByName(httpContext, "GetBook", new { isbn = book.Isbn })!;
-      return Results.Created(path, book);
-      // one way
-      //return Results.CreatedAtRoute("GetBook", new { isbn = book.Isbn }, book);
-      //another way that could be a bad hard code
-      //return Results.Created($"/books/{book.Isbn}", book);
-    }).WithName("CreateBook")
-    .Accepts<Book>("application/json")
-    .Produces<Book>(201)
-    .Produces<IEnumerable<ValidationFailure>>(400)
-    .WithTags("Books");
-
-    app.MapGet("books",
-      [Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
-      async (IBookService bookService, string? searchTerm) =>
-    {
-      if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
-      {
-        var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
-        return Results.Ok(matchedBooks);
-      }
-        var books = await bookService.GetAllAsync();
-        return Results.Ok(books);
-    }).WithName("GetBooks")
+    app.MapGet(BaseRoute, GetAllBooksAsync)
+      .WithName("GetBooks")
       .Produces<IEnumerable<Book>>(200)
-      .WithTags("Books");
+      .WithTags(Tag);
 
-    app.MapGet("books/{isbn}",
-      [Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
-      async (string isbn, IBookService bookService) =>
-    {
-      var book = await bookService.GetByIsbnAsync(isbn);
-      return book is not null ? Results.Ok(book) : Results.BadRequest();
-    }).WithName("GetBook")
-    .Produces<Book>(200)
-    .WithTags("Books");
+    app.MapGet($"{BaseRoute}/{{isbn}}", GetBookByIsbn)
+      .WithName("GetBook")
+      .Produces<Book>(200)
+      .WithTags(Tag);
 
-    app.MapPut("books/{isbn}",
-      [Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
-      async (string isbn, Book book, IBookService bookService,
-      IValidator<Book> validator) =>
-    {
-      book.Isbn = isbn;
-      var validationResult = await validator.ValidateAsync(book);
-      if (!validationResult.IsValid)
-      {
-        return Results.BadRequest(validationResult.Errors);
-      }
-
-        var updated = await bookService.UpdateAsync(book);
-        return updated ? Results.Ok(book) : Results.NotFound();
-      }).WithName("UpdateBook")
-      .Accepts<Book>("application/json")
+    app.MapPut($"{BaseRoute}/{{isbn}}", UpdateBookAsync)
+      .WithName("UpdateBook")
+      .Accepts<Book>(ContentType)
       .Produces<Book>(200)
       .Produces<IEnumerable<ValidationFailure>>(400)
-      .WithTags("Books");
+      .WithTags(Tag);
 
-    app.MapDelete("books/{isbn}",
-      [Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
-      async (string isbn, IBookService bookService) =>
-    {
-      var deleted = await bookService.DeleteAsync(isbn);
-      return deleted ? Results.NoContent() : Results.NotFound();
-    }).WithName("DeleteBook")
-    .Produces(204)
-    .Produces(404)
-    .WithTags("Books");
+    app.MapDelete($"{BaseRoute}/{{isbn}}", DeleteBookAsync)
+      .WithName("DeleteBook")
+      .Produces(204)
+      .Produces(404)
+      .WithTags(Tag);
 
     // writing own extensions, ex. html
     app.MapGet("status", [EnableCors("AnyOrigin")] () =>
@@ -118,6 +62,70 @@ public class LibraryEndpoints : IEndpoints
     //.RequireCors("AnyOrigin");// added with attribute above
     //.ExcludeFromDescription();// perfect to remove from swagger
   }
+
+  internal static async Task<IResult> CreateBookAsync(
+    Book book, IBookService bookService,
+    IValidator<Book> validator)
+  {
+    var validationResult = await validator.ValidateAsync(book);
+    if (!validationResult.IsValid)
+    {
+      return Results.BadRequest(validationResult.Errors);
+    }
+
+    var created = await bookService.CreateAsync(book);
+    if (!created)
+    {
+      return Results.BadRequest(new List<ValidationFailure>
+      {
+        new ("Isbn","A book with this ISBN-13 already exists")
+      });
+    }
+
+    return Results.Created($"/{BaseRoute}/{book.Isbn}", book);
+  }
+
+  internal static async Task<IResult> GetAllBooksAsync(
+    IBookService bookService, string? searchTerm)
+  {
+    if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
+    {
+      var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
+      return Results.Ok(matchedBooks);
+    }
+    var books = await bookService.GetAllAsync();
+    return Results.Ok(books);
+  }
+
+  internal static async Task<IResult> GetBookByIsbn(
+    string isbn, IBookService bookService)
+  {
+    var book = await bookService.GetByIsbnAsync(isbn);
+    return book is not null ? Results.Ok(book) : Results.BadRequest();
+  }
+
+  internal static async Task<IResult> UpdateBookAsync(
+    string isbn, Book book,
+    IBookService bookService,IValidator<Book> validator)
+  {
+    book.Isbn = isbn;
+    var validationResult = await validator.ValidateAsync(book);
+    if (!validationResult.IsValid)
+    {
+      return Results.BadRequest(validationResult.Errors);
+    }
+
+    var updated = await bookService.UpdateAsync(book);
+    return updated ? Results.Ok(book) : Results.NotFound();
+  }
+
+  internal static async Task<IResult> DeleteBookAsync(
+    string isbn, IBookService bookService)
+  {
+    var deleted = await bookService.DeleteAsync(isbn);
+    return deleted ? Results.NoContent() : Results.NotFound();
+  }
+
   public static void AddServices(IServiceCollection services,
     IConfiguration configuration)
   {
